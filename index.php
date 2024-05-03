bf5ab85400a49b54206ffb0e4f1b745f9a7b5309
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-04 01:08:14 +0200

    get data with $_GET

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <form action="badWords.php" method="GET">
+    <textarea name="paragraph" type="text"></textarea>
+    <input name="word" type="text">
+    <button type="submit">invia dati</button>
+ </form>
+</body>
+</html>
